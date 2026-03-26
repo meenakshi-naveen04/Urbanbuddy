eef1ea4251a8cec5a3b5b0cd9863d1f966742528
meenakshi-naveen04 <meenakshimukkath@gmail.com>
2026-03-26 15:35:29 +0530

    second

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import App from './App.jsx'; // This imports the file you just showed me
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 //REVIEW AND RATING
 
